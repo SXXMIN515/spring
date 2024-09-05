832fcec2716af38288d65ee4241a5ff8337a3b3d
admin <admin@YD02-06>
2024-09-05 18:00:21 +0900

    0905

---
+++ boot01/src/main/java/com/yedam/app/dept/service/DeptServiceImpl.java
@@ -0,0 +1,30 @@
+package com.yedam.app.dept.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yedam.app.dept.mapper.DeptMapper;
+@Service
+public class DeptServiceImpl implements DeptService {
+	private DeptMapper deptMapper;
+	
+	@Autowired
+	DeptServiceImpl(DeptMapper deptMapper) {
+		this.deptMapper = deptMapper;
+	}
+
+	@Override
+	public List<DeptVO> deptList() {
+		return deptMapper.selectDeptAll();
+	}
+
+	@Override
+	public DeptVO selectDeptInfo(DeptVO deptVO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
